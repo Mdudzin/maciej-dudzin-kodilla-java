dc551a8c59f12e32d5315da89d9278116c5fc3d7
Dudzin <maciej.dudzin@gmail.com>
2017-08-20 17:14:41 +0200

    Rozwiniecie klasy testujacej

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,12 +1,66 @@
 package com.kodilla.testing.collection;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class CollectionTestSuite {
 
-    public void testOddNumbersExterminatorEmptyList {
+    @Before
+    public void before() {
+
+        System.out.println("Test Case: begin");
 
     }
 
-    public void testOddNumbersExterminatorNormalList {
+    @After
+    public void after() {
+
+        System.out.println("Test Case: end");
+
+    }
+
+    @Test
+    public void testOddNumbersExterminatorEmptyList() {
+
+        //Given//
+        ArrayList<Integer> emptyList = new ArrayList<Integer>();
+
+        OddNumbersExterminator exterminator = new OddNumbersExterminator();
+
+        //When//
+        exterminator.exterminate(emptyList);
+
+        //Then//
+        Assert.assertTrue();
+
+
+    }
+
+    @Test
+    public void testOddNumbersExterminatorNormalList() {
+
+        //Given//
+        ArrayList<Integer> theList = new ArrayList<Integer>();
+
+        OddNumbersExterminator exterminator = new OddNumbersExterminator();
+
+        Random theGenerator = new Random();
+
+        //When//
+        for(int n=0; n<100; n++){
+
+            theList.add(theGenerator.nextInt(100));
+
+        }
+
+        exterminator.exterminate(theList);
+
+        //Then//
+        Assert.assertTrue();
 
     }
 }
