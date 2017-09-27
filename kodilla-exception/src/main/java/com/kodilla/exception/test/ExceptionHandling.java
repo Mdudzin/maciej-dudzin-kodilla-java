1592dbbf230a00c9d6bc7a622a42a727a29f4e26
Dudzin <maciej.dudzin@gmail.com>
2017-09-27 13:30:05 +0200

    Module 8

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,4 +1,18 @@
 package com.kodilla.exception.test;
 
 public class ExceptionHandling {
+
+    public static void main (String[] args) {
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+            secondChallenge.probablyIWillThrowException(3, 2);
+
+        } catch (Exception e) {
+
+            System.err.println("Generic error! " + e);
+        }
+
+    }
 }
